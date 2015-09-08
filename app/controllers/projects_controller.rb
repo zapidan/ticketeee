8ed183246b2e0e9b126f06caa3b42a16f1dd8fe0
Daniela Zapata <dzapatariesco@gmail.com>
2015-09-08 16:04:36 -0500

    Handle error gracefuly in case a user tries to access a project that does not exist, redirect to index page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
@@ -23,15 +25,12 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update(project_params)
       redirect_to @project, notice: "Project has been updated."
     else
@@ -41,14 +40,19 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
 
     redirect_to projects_path, notice: "Project has been deleted."
-
   end
-  
+
   private
+
+  def set_project
+    @project = Project.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
+    redirect_to projects_path
+  end
 
   def project_params
     params.require(:project).permit(:name, :description)
