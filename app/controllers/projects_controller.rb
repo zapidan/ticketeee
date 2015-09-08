3bb3609a9a778ff34bf862c7b1611157b95e2d65
Daniela Zapata <dzapatariesco@gmail.com>
2015-09-08 09:53:20 -0500

    Create a new project feature complete

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,31 @@
+class ProjectsController < ApplicationController
+  def index
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+      # redirect_to @project, flash: { success: "Project has been created." }
+      # redirect_to @project, success: "Project has been created." }
+    else
+      # TODO
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
+end
