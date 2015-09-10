c47ec2646a3966b7eb837272bd639ef73a9652c5
Daniela Zapata <dzapatariesco@gmail.com>
2015-09-09 19:14:19 -0500

    Add sign in and sign out

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,6 +25,9 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Warden::Test::Helpers, type: :feature
+  config.after(type: :feature) { Warden.test_reset! }
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
