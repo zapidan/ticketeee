025953414f37f2bf3015fbfa17b018f76d8c0df4
Daniela Zapata <dzapatariesco@gmail.com>
2015-09-16 21:04:10 -0500

    Serve uploaded attachments through the AttachmentsController for extra security

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,8 @@ Rails.application.routes.draw do
     resources :tickets
   end
 
+  resources :attachments, only: :show
+
 
   # Example resource route with more complex sub-resources:
   #   resources :products do
