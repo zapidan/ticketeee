b9bfb9e00ccf748175e9ba1c8244343b2ad50675
Daniela Zapata <dzapatariesco@gmail.com>
2015-09-20 17:29:41 -0500

    Define available roles in one place only to avoid future inconsistencies

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,7 +2,12 @@ class Role < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
-  def self.available_roles
-    %w(manager editor viewer)
+ # Returns an array of strings or symbols with available roles
+  def self.available_roles(options = {})
+    symbolArray = options[:symbolize] || false
+    available_roles = %w(manager editor viewer)
+    return available_roles.map(&:to_sym) if (symbolArray)
+
+    available_roles
   end
 end
