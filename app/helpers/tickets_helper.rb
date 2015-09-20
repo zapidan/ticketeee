824ad55f84b6429c92d374d4db9006f646578e14
Daniela Zapata <dzapatariesco@gmail.com>
2015-09-20 15:45:15 -0500

    Fix bug: when users with no permission to change state add a comment, state is nil, breaking the view

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,6 +1,6 @@
 module TicketsHelper
   def state_transition_for(comment)
-    if comment.previous_state != comment.state
+    if comment.state && (comment.previous_state != comment.state)
       content_tag(:p) do
         value = "<strong><i class='fa fa-gear'></i> state changed</strong>"
         if comment.previous_state.present? 
