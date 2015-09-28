11e1643b5c0bc000cd4c6c7ba4f248abaa7d3a2b
Daniela Zapata <dzapatariesco@gmail.com>
2015-09-28 14:16:20 -0500

    Add Figaro and configure sending emails in dev with gmail

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem "rails_12factor", group: :production
 gem "puma", group: :production
 gem "active_model_serializers", "~> 0.9.3"
 gem "sinatra"
+gem "figaro", group: :development
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,21 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
+  host = 'localhost:3000'                       # Local Server
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.perform_deliveries = true
+  # SMTP settings for gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:               "smtp.gmail.com",
+    port:                  587,
+    user_name:             ENV["gmail_username"],
+    password:              ENV["gmail_password"],
+    authentication:        "plain",
+    enable_starttls_auto:  true }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
